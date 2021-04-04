7e9486927d37f05ac07e66bbf7d0faca5741d6ca
hgfgdsy <1031010120@qq.com>
2021-04-04 18:42:04 +0800

    4.4

--- fileread.py
+++ fileread.py
@@ -66,6 +66,104 @@ def get_headers():
     return headers
 
 
+def get_last_version(fullname):
+    headers = get_headers()
+    repo_name_list = fullname.split('/')
+    repo_name = repo_name_list[0] + '/' + repo_name_list[1]
+    subdir_name = ''
+    c = 0
+    for n in repo_name_list:
+        c = c + 1
+        if c > 2:
+            subdir_name = subdir_name + '/' + n
+    if subdir_name:
+        # print('1.:', repo_name, subdir_name, '************************************get_releases_url*******')
+        d_url = 'https://api.github.com/repos/' + repo_name
+    else:
+        d_url = 'https://api.github.com/repos/' + fullname
+    try:
+        one_page_results = get_results(d_url, headers)
+        releases_url = one_page_results['releases_url']
+        (v_name, semantic) = get_version(releases_url)
+    except Exception as exp:
+        print("************** get search releases_url error", exp, '*******************************************')
+        v_name = 'master'
+        semantic = False
+    return v_name, semantic
+
+
+def get_version(releases_url):
+    headers = get_headers()
+    v_url = releases_url.replace('{/id}', '')
+    version_result = get_results(v_url, headers)
+    # v_id = ''
+    v_name = ''
+    semantic = True
+    if version_result:
+        v_url = releases_url.replace('{/id}', '/latest')
+        try:
+            result = get_results(v_url, headers)
+        except Exception as exp:
+            result = version_result[0]
+            print("When find version: get search error", exp, '-------------------------------------------------------')
+        v_name = result['tag_name']
+    else:
+        semantic = False
+    return v_name, semantic
+
+
+def get_last_hash(repo_name):
+    repo_name_list = repo_name.split('/')
+    fullname = repo_name_list[0] + '/' + repo_name_list[1]
+    # https://api.github.com/repos/robfig/cron/commits
+    url = 'https://api.github.com/repos/' + fullname + '/commits'
+    headers = get_headers()
+    try:
+        commts = get_results(url, headers)
+        last_commt = commts[0]["sha"][0:7]
+        # print('%%%%get the last commit hash is:', last_commt, fullname)
+    except Exception as exp:
+        last_commt = ''
+        print("************** get search releases_url error", exp, '*******************************************')
+    return last_commt
+
+
+# get hash through version, 获取一个版本对应的哈希值，需要补充一个通过哈希值获取版本的方法 ~~~
+def get_hash(url, search_e):
+    v_hash = ''
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0'
+    }
+    try:
+        response = requests.get(url, headers=headers)
+        content = response.content.decode('utf-8')
+        soup = BeautifulSoup(content, "lxml")
+        # f6 link-gray text-mono ml-2 d-none d-lg-inline
+        div_msg = str(soup.find('a', class_='f6 link-gray text-mono ml-2 d-none d-lg-inline'))
+        div_msg = div_msg.strip('').replace('\n', '')
+        # print(div_msg)
+        hash_str = re.findall(r">([0-9a-zA-Z]+?)</a>", div_msg)
+        # print(hash_str)
+        if hash_str:
+            v_hash = hash_str[0]
+    except Exception as exp:
+        search_e = search_e + 1
+        print("get hash error:", exp, "**************************************************")
+    return v_hash, search_e
+
+
+def get_last_version_or_hashi(repo_name, search_e):
+    v_name = ''
+    v_hashi = ''
+    (v_name, semantic) = get_last_version(repo_name)
+    if not semantic:
+        v_hashi = get_last_hash(repo_name)
+    else:
+        url = "https://github.com/" + repo_name + '/tree/' + v_name
+        (v_hash, search_e) = get_hash(url, search_e)
+    return v_name, v_hashi, search_e
+
+
 def check_repo_exist_web(repo_name):
     url = 'https://github.com/' + repo_name.replace('github.com/', '').strip()
     headers = {
@@ -352,6 +450,91 @@ def download_a_repo(repo, version):
 
 
 def write_modify_to_mod(modifies):
+    repos = []
+    dic = {}
+    for m in modifies:
+        repos.append(m[0])
+        dic[m[0]] = m[1]
+    f = open('./pkg/hgfgdsy=migtry@v0.0.0/go.mod', 'r')
+
+    go_mod_content = f.read()
+    require_part = go_mod_content.replace('"', '')
+    f.close()
+    requires_list = []
+    mod_requires = re.findall(r"require\s*\(\n*(.+?)\n*\)", require_part, re.S)  # 括号括起来的requires
+    if mod_requires:
+        require_l = mod_requires[0].split('\n')
+        for require_r in require_l:
+            require_r = require_r.strip().replace('+incompatible', '')
+            # (not re.findall(r"^[0-9a-zA-Z]+?/[0-9a-zA-Z]+?$", require_r))
+            #                 and (not re.findall(r"^[0-9a-zA-Z]+?$", require_r)) and
+            if require_r and (not re.findall(r"^//.+?", require_r)) and (require_r not in requires_list):
+                requires_list.append(require_r)
+                # print(require_r)
+    mod_requires = re.findall(r"^require\s+([^(]+?)$", require_part, re.M)  # 不是括号括起来的requires
+    for require_r in mod_requires:
+        require_r = require_r.strip().replace('+incompatible', '')
+        if require_r and (require_r not in requires_list):
+            requires_list.append(require_r)
+
+
+    ansr = []
+    for r in requires_list:
+        temp = r.split()
+        if temp[0] in repos:
+            msg = temp[0] + ' ' + dic[temp[0]]
+            ansr.append(msg)
+        else:
+            ansr.append(r)
+
+
+    tag = 0
+    msg = ''
+    lines = go_mod_content.split('\n')
+    for line in lines:
+
+        if re.findall(r'^require\s*', line):
+            tag = 1
+
+        if tag == 0:
+            msg = msg + line + '\n'
+
+        if tag == 1:
+            if re.findall(r"^replace", line):
+                tag = 2
+                msg = msg + 'require' + ' (' + '\n'
+                for r in ansr:
+                    msg = msg + r + '\n'
+                msg = msg + ')\n'
+            else:
+                continue
+        if tag == 2:
+            msg = msg + line + '\n'
+    f = open('./pkg/hgfgdsy=migtry@v0.0.0/go.mod', 'w')
+    f.write(msg)
+    f.close()
+
+    # get all require
+    # mod_requires = re.findall(r"require\s*\(\n*(.+?)\n*\)", require_part, re.S)  # 括号括起来的requires
+    # if mod_requires:
+    #     require_l = mod_requires[0].split('\n')
+    #     for require_r in require_l:
+    #         require_r = require_r.strip().replace('+incompatible', '')
+    #
+    #         # (not re.findall(r"^[0-9a-zA-Z]+?/[0-9a-zA-Z]+?$", require_r))
+    #         #                 and (not re.findall(r"^[0-9a-zA-Z]+?$", require_r)) and
+    #         if require_r and (not re.findall(r"^//.+?", require_r)):
+    #             rp = require_r[0]
+    #             if rp in repos:
+    #
+    #             # print(require_r)
+    # mod_requires = re.findall(r"^require\s+([^(]+?)$", require_part, re.M)  # 不是括号括起来的requires
+    # for require_r in mod_requires:
+    #     require_r = require_r.strip().replace('+incompatible', '')
+    #     if require_r and (require_r not in requires_list):
+    #         requires_list.append(require_r)
+
+
     return
 
 
